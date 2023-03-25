064137253594eab6c52450a50d5ee899711e5d44
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-25 08:27:27 +0000

    Designed models for database tables

---
+++ Models/BodypartModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace CourseworkApp.Models
+{
+    [Table("Bodyparts")]
+    class BodypartModel
+    {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int Id { get; set; }
+
+        [MaxLength(250), Column("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/ExerciseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace CourseworkApp.Models
+{
+    [Table("Exercises")]
+    class ExerciseModel
+    {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int Id { get; set; }
+        
+        [MaxLength(250), Column("name")]
+        public string Name { get; set; }
+
+        [MaxLength(250), Column("instructions")]
+        public string Instructions { get; set; }
+
+        [MaxLength(250), Column("imageURL")]
+        public string ImageURL { get; set; }
+
+        [Column("bodypartID")]
+        public int BodypartId { get; set; }
+    }
+}
