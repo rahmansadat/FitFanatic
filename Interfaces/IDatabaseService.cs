22723dfaba4d6ab6f3955d21c67cedea47aa0b22
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-26 00:55:55 +0000

    Designed interface for database interaction

---
+++ Interfaces/IDatabaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseworkApp.Models;
+
+namespace CourseworkApp.Interfaces
+{
+    public interface IDatabaseService
+    {
+        List<ExerciseModel> GetExercises();
+        List<ExerciseModel> GetExercisesByBodypart(int bodypartID);
+        List<BodypartModel> GetBodyparts();
+    }
+}
