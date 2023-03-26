274237e1db8eabc732a5e1df34950975796d20db
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-26 19:02:12 +0100

    Set up base viewmodel that defines OnPropertyChanged

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CourseworkApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        //int rep1;
+
+        //public int Rep1
+        //{
+        //    get => rep1;
+        //    set
+        //    {
+        //        rep1 = value;
+        //        OnPropertyChanged(nameof(rep1));
+        //    }
+        //}
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public BaseViewModel()
+        {
+        }
+    }
+}
