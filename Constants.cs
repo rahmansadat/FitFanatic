7bb02f3bb29824111cf4bd1513287bbd54649437
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-26 18:57:02 +0100

    Set up path for workout log file

--- Constants.cs
+++ Constants.cs
@@ -8,8 +8,7 @@ namespace CourseworkApp
 {
     class Constants
     {
-        //public const string DatabaseFilename = "appDatabase.db3";
-        public const string DatabaseFilename = "../ffdatabase.db3";
+        public const string DatabaseFilename = "appDatabase.db3";
 
         public const SQLite.SQLiteOpenFlags Flags =
             // open the database in read/write mode
@@ -19,8 +18,12 @@ namespace CourseworkApp
             // enable multi-threaded database access
             SQLite.SQLiteOpenFlags.SharedCache;
 
+        public const string WorkoutLogFilename = "workoutLog.txt";
+
         public static string DatabasePath => 
             Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
 
+        public static string WorkoutLogPath =>
+            Path.Combine(FileSystem.AppDataDirectory, WorkoutLogFilename);
     }
 }
