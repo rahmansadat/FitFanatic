5ea705ac11494806417810b678e8060b060c91ca
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-26 18:59:49 +0100

    Registered singletons LogWorkoutPage and LogWorkoutPageViewModel

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -32,13 +32,13 @@ public static class MauiProgram
         //Pages
         //mauiAppBuilder.Services.AddTransient(typeof(Views.ExercisesPage));
         //mauiAppBuilder.Services.AddTransient(typeof(Views.HistoryPage));
-        //mauiAppBuilder.Services.AddTransient(typeof(Views.LogWorkoutPage));
+        mauiAppBuilder.Services.AddSingleton(typeof(Views.LogWorkoutPage));
         //mauiAppBuilder.Services.AddTransient(typeof(Views.ScorePage));
 
         //Services
         //mauiAppBuilder.Services.AddTransient(typeof(ViewModels.ExercisesPageViewModel));
         //mauiAppBuilder.Services.AddTransient(typeof(ViewModels.HistoryPageViewModel));
-        //mauiAppBuilder.Services.AddTransient(typeof(ViewModels.LogWorkoutPageViewModel));
+        mauiAppBuilder.Services.AddSingleton(typeof(ViewModels.LogWorkoutPageViewModel));
         //mauiAppBuilder.Services.AddTransient(typeof(ViewModels.ScorePageViewModel));
 
 
