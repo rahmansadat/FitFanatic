6d02581c29099f2ecbe26393bded85c42cc1cc4a
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-27 21:01:51 +0100

    Designed model for quotes

---
+++ Models/QuoteModel.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseworkApp.Models
+{
+    public class QuoteModel
+    {
+        public string Author { get; set; } = null;
+        public string Quote { get; set; } = null;
+
+        public QuoteModel()
+        {
+        }
+
+        public QuoteModel(string author, string quote)
+        {
+            Author = author;
+            Quote = quote;
+        }
+    }
+}
