ab28a3813a07a836342555087ff70bcc4778a874
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-27 11:18:35 +0100

    Designed viewmodel for ExercisesPage

---
+++ ViewModels/ExercisesPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseworkApp.Interfaces;
+using CourseworkApp.Models;
+
+namespace CourseworkApp.ViewModels
+{
+    public class ExercisesPageViewModel : BaseViewModel
+    {
+        public Models.LogWorkoutPageModel LogWorkoutPageModel { get; set; }
+        private readonly IDatabaseService database;
+        public ObservableCollection<ExerciseModel> Items { get; private set; }
+
+        public ExercisesPageViewModel(IDatabaseService database)
+        {
+            this.database = database;
+            var res = database.GetExercises();
+            Items = new ObservableCollection<ExerciseModel>(res);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
