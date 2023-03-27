80b482351e8ad3d1ddce32eca571ceae63742ab0
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-27 21:18:25 +0100

    Removed unnecessary constants

--- Constants.cs
+++ Constants.cs
@@ -10,20 +10,7 @@ namespace CourseworkApp
     {
         public static string apiEndpoint = "https://api.fisenko.net/v1/quotes/en/random";
 
-        public const string DatabaseFilename = "appDatabase.db3";
-
-        public const SQLite.SQLiteOpenFlags Flags =
-            // open the database in read/write mode
-            SQLite.SQLiteOpenFlags.ReadWrite |
-            // create the database if it doesn't exist
-            SQLite.SQLiteOpenFlags.Create |
-            // enable multi-threaded database access
-            SQLite.SQLiteOpenFlags.SharedCache;
-
         public const string WorkoutLogFilename = "workoutLog.txt";
-
-        public static string DatabasePath => 
-            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
 
         public static string WorkoutLogPath =>
             Path.Combine(FileSystem.AppDataDirectory, WorkoutLogFilename);
