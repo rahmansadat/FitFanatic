6250118a520ae284fb753f56b3587d631e84a337
Sadat Rahman <sadatrahmanbus@gmail.com>
2023-03-27 21:12:08 +0100

    Designed viewmodel for ScorePage

---
+++ ViewModels/ScorePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CourseworkApp.Interfaces;
+using CourseworkApp.Models;
+using CourseworkApp.Views;
+
+namespace CourseworkApp.ViewModels
+{
+    public class ScorePageViewModel : BaseViewModel
+    {
+        public ScorePageViewModel()
+        {
+        }
+
+    }
+}
